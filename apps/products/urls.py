d5252d352763d452d8292571cf23b47f646a8218
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-06 11:09:16 +0545

    feat: create new product

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ProductList.as_view(), name='product_list'),
-    path('<int:pk>/', views.ProductDetail.as_view(), name='product_detail')
+    path('<int:pk>/', views.ProductDetail.as_view(), name='product_detail'),
+    path('create/', views.ProductCreate.as_view(), name='product_create')
 ]
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,6 +1,8 @@
 from django.views import generic
+from django.urls import reverse
 
 from .models import Product
+from .forms import ProductForm
 
 
 class ProductList(generic.ListView):
@@ -14,3 +16,10 @@ class ProductDetail(generic.DetailView):
     template_name = 'products/product_detail.html'
     context_object_name = 'product'
 
+
+class ProductCreate(generic.CreateView):
+    form_class = ProductForm
+    template_name = 'products/product_create.html'
+
+    def get_success_url(self):
+        return reverse('product_detail', kwargs={'pk': self.object.pk})
