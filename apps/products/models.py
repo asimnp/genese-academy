6c8680b24d3d643bff4485b885514260c363675b
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-06 08:24:54 +0545

    fix: app name, create abstract, category, product model

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class DateTimeModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(DateTimeModel):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    category = models.ForeignKey(Category, models.SET_NULL, blank=True, null=True, related_name='category')
+
+    class Meta:
+        ordering = ('category',)
+
+    def __str__(self):
+        return self.name
