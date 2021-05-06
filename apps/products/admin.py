4c453dc75619f4a03b9b35a5ef79a61c2393832b
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-06 08:32:31 +0545

    register category, product model to admin site

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category']
+    list_filter = ['name', 'category']
+    search_fields = ('name',)
+    list_editable = ['price']
+
+
+admin.site.register(Category)
