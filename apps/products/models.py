045d58800338bd7b9c13e7404426b15dd80170e6
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-06 08:34:06 +0545

    rename category model plural name

--- apps/products/models.py
+++ apps/products/models.py
@@ -12,6 +12,9 @@ class DateTimeModel(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name_plural = 'categories'
+
     def __str__(self):
         return self.name
 
